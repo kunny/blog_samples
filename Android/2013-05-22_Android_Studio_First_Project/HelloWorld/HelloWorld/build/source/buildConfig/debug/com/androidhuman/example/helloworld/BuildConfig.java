e3d49f138da0dec331ffa181a7de82ed84eda749
Taeho Kim <jyte82@gmail.com>
2013-05-22 10:40:55 +0900

    Android studio sample

---
+++ Android/2013-05-22_Android_Studio_First_Project/HelloWorld/HelloWorld/build/source/buildConfig/debug/com/androidhuman/example/helloworld/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.androidhuman.example.helloworld;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
