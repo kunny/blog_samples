50f17ea0720354a0e33d442a46103ed06a8f84c9
Taeho Kim <jyte82@gmail.com>
2015-01-27 19:30:49 +0900

    Fix lint error for 'Material design for everyone'
    Signed-off-by: Taeho Kim <jyte82@gmail.com>

--- Android/2015-01-24_Material_Design_for_everyone/app/src/main/java/com/androidhuman/example/materialdesign/LowProfileActivity.java
+++ Android/2015-01-24_Material_Design_for_everyone/app/src/main/java/com/androidhuman/example/materialdesign/LowProfileActivity.java
@@ -25,6 +25,7 @@ public class LowProfileActivity extends ActionBarActivity {
     ArgbEvaluator argbEvaluator;
 
     @Override
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_low_profile);
