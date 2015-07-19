bf01a820d88ef6f65583f1d441414a5f35981618
Taeho Kim <jyte82@gmail.com>
2015-07-19 22:53:16 +0900

    Update 'NavigationDrawer-Full Overlay' sample
    Signed-off-by: Taeho Kim <jyte82@gmail.com>

--- Android/2014-11-17_Material_Navigation_Drawer_FullOverlay/app/src/main/java/com/androidhuman/example/materialdrawer/fulloverlay/MainActivity.java
+++ Android/2014-11-17_Material_Navigation_Drawer_FullOverlay/app/src/main/java/com/androidhuman/example/materialdrawer/fulloverlay/MainActivity.java
@@ -1,20 +1,22 @@
 package com.androidhuman.example.materialdrawer.fulloverlay;
 
 import android.content.res.Configuration;
-import android.graphics.Color;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     DrawerLayout dlDrawer;
+    NavigationView nvNavigation;
     ActionBarDrawerToggle dtToggle;
 
     @Override
@@ -23,9 +25,15 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //toolbar.setNavigationIcon(R.drawable.ic_menu);
+
         dlDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        if (null != ab) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
 
         dtToggle = new ActionBarDrawerToggle(this, dlDrawer, R.string.app_name, R.string.app_name);
         dlDrawer.setDrawerListener(dtToggle);
