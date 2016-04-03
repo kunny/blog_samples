8eb6462f3fe5583247e6da1b6d0a4b13558e7c56
Taeho Kim <jyte82@gmail.com>
2016-04-03 17:43:39 +0900

    Project: Google Maps V2 Android_2016
    Signed-off-by: Taeho Kim <jyte82@gmail.com>

---
+++ Android/2016-03-26_Google_Maps_V2_2016/app/src/main/java/com/androidhuman/example/googlemaps/MapsActivity.java
@@ -0,0 +1,12 @@
+package com.androidhuman.example.googlemaps;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MapsActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
